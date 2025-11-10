43a7aa33c0309c48270e69fddfda980c68d9d175
Ram <ramdyalgupta120@gmail.com>
2025-11-10 17:43:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4601302081-ae02a",
+  "appId": "1:679307333505:web:6df9d23ccf05afd2e9e280",
+  "apiKey": "AIzaSyAaGLIGYImDR5-oR3F_2tps8gkk7FK89yU",
+  "authDomain": "studio-4601302081-ae02a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "679307333505"
+};
